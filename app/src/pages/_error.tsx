933c596f34e1b265d42680ff3204a4a411041ea3
Adrian Parker <ihsw.aparker@gmail.com>
2019-10-19 16:44:20 -0700

    Fix error page.

--- app/src/pages/_error.tsx
+++ app/src/pages/_error.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import { NotFound } from "@sotah-inc/client/build/dist/components/util/NotFound";
 import HttpStatus from "http-status";
-import { Layout } from "../components/Layout";
 
 interface IOwnProps {
   statusCode?: number;
@@ -21,32 +20,16 @@ class Error extends React.Component<Props> {
     const { statusCode } = this.props;
 
     if (typeof statusCode === "undefined") {
-      return (
-        <Layout title="Secrets of the Auction House">
-          <p>An error occurred!</p>
-        </Layout>
-      );
+      return <p>An error occurred!</p>;
     }
 
     switch (statusCode) {
       case HttpStatus.NOT_FOUND:
-        return (
-          <Layout title="Secrets of the Auction House">
-            <NotFound />
-          </Layout>
-        );
+        return <NotFound />;
       case HttpStatus.INTERNAL_SERVER_ERROR:
-        return (
-          <Layout title="Secrets of the Auction House">
-            <p>An internal server error occurred!</p>
-          </Layout>
-        );
+        return <p>An internal server error occurred!</p>;
       default:
-        return (
-          <Layout title="Secrets of the Auction House">
-            <p>An error {statusCode} occurred!</p>
-          </Layout>
-        );
+        return <p>An error {statusCode} occurred!</p>;
     }
   }
 }

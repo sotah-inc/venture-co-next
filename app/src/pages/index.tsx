d9df88997806e44dfe68278586e519f851b3dbcf
Adrian Parker <ihsw.aparker@gmail.com>
2019-10-19 16:43:08 -0700

    Start updating pages to use new entry-point components.

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,37 +1,19 @@
 import React from "react";
 
-import { ReceiveGetBoot, ReceiveGetPing } from "@sotah-inc/client/build/dist/actions/main";
-import { getBoot } from "@sotah-inc/client/build/dist/api/data";
-import { runners } from "@sotah-inc/client/build/dist/reducers/handlers";
-import { RootRouteContainer } from "@sotah-inc/client/build/dist/route-containers/App/Root";
-import { defaultMainState, IStoreState } from "@sotah-inc/client/build/dist/types";
+import { getBoot, getPing } from "@sotah-inc/client/build/dist/api/data";
+import { RootRouteContainer } from "@sotah-inc/client/build/dist/route-containers/entry-point/Root";
 import { IGetBootResponse } from "@sotah-inc/core";
 import { NextPageContext } from "next";
 
 interface IInitialProps {
   data?: {
+    ping: boolean;
     boot: IGetBootResponse | null;
   };
 }
 
 export function Home({ data }: Readonly<IInitialProps>) {
-  const predefinedState: Partial<IStoreState> | undefined = (() => {
-    if (typeof data === "undefined") {
-      return;
-    }
-
-    return {
-      Main: runners.main(
-        runners.main(defaultMainState, ReceiveGetPing(true)),
-        ReceiveGetBoot(data.boot),
-      ),
-    };
-  })();
-
-  // tslint:disable-next-line:no-console
-  console.log("predefinedState", predefinedState);
-
-  return <RootRouteContainer />;
+  return <RootRouteContainer rootEntrypointData={data} />;
 }
 
 Home.getInitialProps = async ({ req }: NextPageContext): Promise<IInitialProps> => {
@@ -39,7 +21,7 @@ Home.getInitialProps = async ({ req }: NextPageContext): Promise<IInitialProps> 
     return {};
   }
 
-  return { data: { boot: await getBoot() } };
+  return { data: { boot: await getBoot(), ping: await getPing() } };
 };
 
 export default Home;

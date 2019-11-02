90c14a3389fee09892c809e5afbafb472945755a
Adrian Parker <ihsw.aparker@gmail.com>
2019-11-02 13:54:58 -0700

    Clean up.

--- app/src/pages/content/news/[post_slug].tsx
+++ app/src/pages/content/news/[post_slug].tsx
@@ -1,59 +1,23 @@
-import { ReceiveGetPost } from "@sotah-inc/client/build/dist/actions/posts";
 import React from "react";
 
-import { ReceiveGetBoot, ReceiveGetPing } from "@sotah-inc/client/build/dist/actions/main";
-import { getBoot } from "@sotah-inc/client/build/dist/api/data";
 import { getPost } from "@sotah-inc/client/build/dist/api/posts";
-import { runners } from "@sotah-inc/client/build/dist/reducers/handlers";
-import { PostRouteContainer } from "@sotah-inc/client/build/dist/route-containers/App/Content/Post";
-import {
-  defaultMainState,
-  defaultPostsState,
-  IStoreState,
-} from "@sotah-inc/client/build/dist/types";
+// tslint:disable-next-line:max-line-length
+import { PostRouteContainer } from "@sotah-inc/client/build/dist/route-containers/entry-point/Content/Post";
 import { extractString } from "@sotah-inc/client/build/dist/util";
 import { IGetPostResponse } from "@sotah-inc/core";
-import { IGetBootResponse } from "@sotah-inc/server/build/dist/messenger/contracts";
 import { NextPageContext } from "next";
 
-import { Layout } from "../../../components/Layout";
-
 interface IInitialProps {
-  data?: {
-    boot: IGetBootResponse | null;
-    post: IGetPostResponse | null;
-  };
+  post: IGetPostResponse | null;
 }
 
-export function Creator({ data }: Readonly<IInitialProps>) {
-  const predefinedState: Partial<IStoreState> | undefined = (() => {
-    if (typeof data === "undefined") {
-      return;
-    }
-
-    return {
-      Main: runners.main(
-        runners.main(defaultMainState, ReceiveGetPing(true)),
-        ReceiveGetBoot(data.boot),
-      ),
-      Posts: runners.post(defaultPostsState, ReceiveGetPost(data.post)),
-    };
-  })();
-
-  return (
-    <Layout title="Secrets of the Auction House" predefinedState={predefinedState}>
-      <PostRouteContainer />
-    </Layout>
-  );
+export function Creator({ post: _post }: Readonly<IInitialProps>) {
+  return <PostRouteContainer />;
 }
 
-Creator.getInitialProps = async ({ req, query }: NextPageContext): Promise<IInitialProps> => {
-  if (typeof req === "undefined") {
-    return {};
-  }
-
+Creator.getInitialProps = async ({ query }: NextPageContext): Promise<IInitialProps> => {
   return {
-    data: { boot: await getBoot(), post: await getPost(extractString("post_slug", query)) },
+    post: await getPost(extractString("post_slug", query)),
   };
 };
 

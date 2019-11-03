40e12f3617982c59b0a53c657163d7004f4b38d4
Adrian Parker <ihsw.aparker@gmail.com>
2019-11-03 11:34:18 -0700

    Update to latest.

--- app/src/pages/data/[region_name].tsx
+++ app/src/pages/data/[region_name].tsx
@@ -1,23 +1,28 @@
 import React from "react";
 
+import { ILoadRegionEntrypoint } from "@sotah-inc/client/build/dist/actions/main";
 import { getStatus } from "@sotah-inc/client/build/dist/api/data";
 // tslint:disable-next-line:max-line-length
 import { RegionRouteContainer } from "@sotah-inc/client/build/dist/route-containers/entry-point/Region";
 import { extractString } from "@sotah-inc/client/build/dist/util";
-import { IStatusRealm } from "@sotah-inc/core";
 import { NextPageContext } from "next";
 
 interface IInitialProps {
-  realms: IStatusRealm[] | null;
+  payload: ILoadRegionEntrypoint;
 }
 
-export function Region({ realms: _realms }: Readonly<IInitialProps>) {
-  return <RegionRouteContainer />;
+export function Region({ payload }: Readonly<IInitialProps>) {
+  return <RegionRouteContainer regionEntrypointData={payload} />;
 }
 
 Region.getInitialProps = async ({ query }: NextPageContext): Promise<IInitialProps> => {
+  const nextRegionName = extractString("region_name", query);
+
   return {
-    realms: await getStatus(extractString("region_name", query)),
+    payload: {
+      nextRegionName,
+      realms: await getStatus(nextRegionName),
+    },
   };
 };
 

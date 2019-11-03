4f56177a93f5b34c3346f12f966e20607ef6c42c
Adrian Parker <ihsw.aparker@gmail.com>
2019-11-03 11:23:57 -0700

    Move data entry-point component to appropriate directory.

--- app/src/pages/data.tsx
+++ app/src/pages/data.tsx
@@ -1,48 +1,9 @@
 import React from "react";
 
-import { ReceiveGetBoot, ReceiveGetPing } from "@sotah-inc/client/build/dist/actions/main";
-import { getBoot } from "@sotah-inc/client/build/dist/api/data";
-import { runners } from "@sotah-inc/client/build/dist/reducers/handlers";
-import { DataRouteContainer } from "@sotah-inc/client/build/dist/route-containers/App/Data";
-import { defaultMainState, IStoreState } from "@sotah-inc/client/build/dist/types";
-import { IGetBootResponse } from "@sotah-inc/core";
-import { NextPageContext } from "next";
+import { DataRouteContainer } from "@sotah-inc/client/build/dist/route-containers/entry-point/Data";
 
-import { Layout } from "../components/Layout";
-
-interface IInitialProps {
-  data?: {
-    boot: IGetBootResponse | null;
-  };
+export function Data() {
+  return <DataRouteContainer />;
 }
-
-export function Data({ data }: Readonly<IInitialProps>) {
-  const predefinedState: Partial<IStoreState> | undefined = (() => {
-    if (typeof data === "undefined") {
-      return;
-    }
-
-    return {
-      Main: runners.main(
-        runners.main(defaultMainState, ReceiveGetPing(true)),
-        ReceiveGetBoot(data.boot),
-      ),
-    };
-  })();
-
-  return (
-    <Layout title="Secrets of the Auction House" predefinedState={predefinedState}>
-      <DataRouteContainer />
-    </Layout>
-  );
-}
-
-Data.getInitialProps = async ({ req }: NextPageContext): Promise<IInitialProps> => {
-  if (typeof req === "undefined") {
-    return {};
-  }
-
-  return { data: { boot: await getBoot() } };
-};
 
 export default Data;
